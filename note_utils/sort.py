7527ebefe0ec764ed4c792f84663970bc24dda06
ce <cetraerup@gmail.com>
2024-12-23 07:35:31 +0100

    notes

--- note_utils/sort.py
+++ note_utils/sort.py
@@ -11,6 +11,7 @@ from typing import Iterator
 class Price:
     value: int
     additional: str
+    payable: bool = True
 
     @property
     def compare_value(self) -> tuple[int, int]:
@@ -18,8 +19,11 @@ class Price:
 
     def __gt__(self, other) -> bool:
         if isinstance(other, Price):
-            return self.compare_value > other.compare_value
+            return other.payable and (
+                not self.payable or (self.compare_value > other.compare_value)
+            )
         return False
+
 
 @dataclasses.dataclass
 class Creature:
@@ -28,21 +32,28 @@ class Creature:
 
     @classmethod
     def from_str(cls, s: str) -> Creature:
-        return cls(s, Price(int(match.group(1)), match.group(2)) if (match := re.match(r"^[^{]*\{(\d+)([a-z]*)}", s)) else Price(0, ''))
+        return cls(
+            s,
+            Price(int(match.group(1)), match.group(2))
+            if (match := re.match(r"^[^{]*\{(\d+)([a-z]*)}", s))
+            else Price(0, "", False),
+        )
 
     def serialize(self) -> str:
         return self.source
 
 
 def load_creatures() -> Iterator[Creature]:
-    with open('../notes/creatures.txt', 'r') as f:
-        for raw in f.read().split('\n\n'):
+    with open("notes/creatures.txt", "r") as f:
+        for raw in f.read().split("\n\n"):
             yield Creature.from_str(raw.strip())
 
 
 def sort_creatures():
-    result = '\n\n'.join(c.serialize() for c in sorted(load_creatures(), key=lambda c: c.cost))
-    with open('../notes/creatures.txt', 'w') as f:
+    result = "\n\n".join(
+        c.serialize() for c in sorted(load_creatures(), key=lambda c: c.cost)
+    )
+    with open("notes/creatures.txt", "w") as f:
         f.write(result)
 
 
